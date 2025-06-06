7e3a9c98ca5c88aef9bc6400359248b360ac1ebf
milarrah <0110124229@student.nurulfikri.ac.id>
2025-06-06 21:12:22 +0700

    Delete Praktikum10/room-booking directory

--- Praktikum10/room-booking/resources/views/livewire/counter.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="container mx-auto">
-    <h1 class="text-2xl font-bold mb-4">Counter</h1>
-    <div class="flex items-center space-x-4">
-        <span class="text-xl">{{ $count }}</span>
-        <button wire:click="increment">Tambah</button>
-    </div>
-</div>
-
-
